e79534bed10dced1191bd26837314a6507dcaf8f
post64bit <post64bit@gmail.com>
2021-03-12 18:04:51 +0300

    day 5

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 window.onscroll = function showHeader () {
     let header = document.querySelector('.header');
-    let header_item = document.querySelectorAll('.header_item')
+    let header_list = document.querySelector('.header_list');
+    let header_nav = document.querySelector('.header_nav');
     if(window.pageYOffset > 50) {
         header.classList.add('header_fixed');
-        header_item.classList.add('header_link_fixed');
+        header_list.classList.add('header_link_fixed');
+        header_nav.classList.add('header_nav_fixed');
     } else {
         header.classList.remove('header_fixed');
-        header_item.classList.remove('header_link_fixed');
-
+        header_list.classList.remove('header_link_fixed');
+        header_nav.classList.remove('header_nav_fixed');
     }
 }
-
-console.log('hello')
 
