c95ddfbc47322537fb9083f665c2e49721b4351b
post64bit <post64bit@gmail.com>
2021-03-13 20:10:53 +0300

    day 6

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ window.onscroll = function showHeader () {
     let header = document.querySelector('.header');
     let header_list = document.querySelector('.header_list');
     let header_nav = document.querySelector('.header_nav');
-    if(window.pageYOffset > 50) {
+    if ($(document).scrollTop() > parseInt($('nav').css('height')) - 30) {
         header.classList.add('header_fixed');
         header_list.classList.add('header_link_fixed');
         header_nav.classList.add('header_nav_fixed');
@@ -10,6 +10,11 @@ window.onscroll = function showHeader () {
         header.classList.remove('header_fixed');
         header_list.classList.remove('header_link_fixed');
         header_nav.classList.remove('header_nav_fixed');
+    };
+    if ($(document).scrollTop() > 500) {
+        $('.arrow').css('opacity','1');
+    } else {
+        $('.arrow').css('opacity','0');
     }
 }
 
