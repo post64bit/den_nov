df3dd563f3edfe1e9e027fc988208044a5adbdf8
post64bit <post64bit@gmail.com>
2021-03-21 16:31:50 +0300

    added popup

--- js/main.js
+++ js/main.js
@@ -1,52 +1,53 @@
 //header
 
-window.onscroll = function showHeader () {
-    let header = document.querySelector('.header');
-    let header_list = document.querySelector('.header_list');
-    let header_nav = document.querySelector('.header_nav');
-    if ($(document).scrollTop() > parseInt($('nav').css('height')) - 30) {
-        header.classList.add('header_fixed');
-        header_list.classList.add('header_link_fixed');
-        header_nav.classList.add('header_nav_fixed');
-        $('.darkmode_moon').css('fill','white');
-        $('.darkmode_sun').css('fill','white');
+window.onscroll = function showHeader() {
+    let header = document.querySelector(".header");
+    let header_list = document.querySelector(".header_list");
+    let header_nav = document.querySelector(".header_nav");
+    if ($(document).scrollTop() > parseInt($("nav").css("height")) - 30) {
+        header.classList.add("header_fixed");
+        header_list.classList.add("header_link_fixed");
+        header_nav.classList.add("header_nav_fixed");
+        $(".darkmode_moon").css("fill", "white");
+        $(".darkmode_sun").css("fill", "white");
     } else {
-        header.classList.remove('header_fixed');
-        header_list.classList.remove('header_link_fixed');
-        header_nav.classList.remove('header_nav_fixed');
-        if(!document.getElementById('checkbox').checked == true) {
-            $('.darkmode_moon').css('fill','black');
-            $('.darkmode_sun').css('fill','black');
+        header.classList.remove("header_fixed");
+        header_list.classList.remove("header_link_fixed");
+        header_nav.classList.remove("header_nav_fixed");
+        if (!document.getElementById("checkbox").checked == true) {
+            $(".darkmode_moon").css("fill", "black");
+            $(".darkmode_sun").css("fill", "black");
         }
-    };
-    if ($(document).scrollTop() > 500) {
-        $('.arrow').css('opacity','1');
-    } else {
-        $('.arrow').css('opacity','0');
     }
-}
+    if ($(document).scrollTop() > 500) {
+        $(".arrow").css("opacity", "1");
+    } else {
+        $(".arrow").css("opacity", "0");
+    }
+};
 
 //header end
 
 //darkmode
 
-if(!localStorage.theme) localStorage.theme = 'light'
-document.body.className = localStorage.theme
-if(localStorage.theme == 'dark') document.getElementById('checkbox').checked = true
+if (!localStorage.theme) localStorage.theme = "light";
+document.body.className = localStorage.theme;
+if (localStorage.theme == "dark")
+    document.getElementById("checkbox").checked = true;
 
 function checkClick() {
-    if(document.getElementById('checkbox').checked == true) {
-        document.body.classList.add('dark');
-        $('.darkmode_moon').css('fill','white');
-        $('.darkmode_sun').css('fill','white'); 
-        localStorage.theme = 'dark'
+    if (document.getElementById("checkbox").checked == true) {
+        document.body.classList.add("dark");
+        $(".darkmode_moon").css("fill", "white");
+        $(".darkmode_sun").css("fill", "white");
+        localStorage.theme = "dark";
     } else {
-        document.body.classList.remove('dark');
-        if ($(document).scrollTop() < parseInt($('nav').css('height')) - 30) {
-            $('.darkmode_moon').css('fill','black');
-            $('.darkmode_sun').css('fill','black');
+        document.body.classList.remove("dark");
+        if ($(document).scrollTop() < parseInt($("nav").css("height")) - 30) {
+            $(".darkmode_moon").css("fill", "black");
+            $(".darkmode_sun").css("fill", "black");
         }
-        localStorage.theme = 'light'
+        localStorage.theme = "light";
     }
     location.reload();
 }
@@ -55,7 +56,7 @@ function checkClick() {
 
 //stars dark mode !
 
-if(localStorage.theme == 'dark') {
+if (localStorage.theme == "dark") {
     $(".program_stars").attr("src", "./img/svg/stars4_dark.svg");
     $(".program_stars3").attr("src", "./img/svg/stars3_dark.svg");
 }
@@ -64,30 +65,35 @@ if(localStorage.theme == 'dark') {
 
 //translate
 
-window.onhashchange = function() {
+window.onhashchange = function () {
     let hash = window.location.hash;
     hash = hash.substr(1);
-    if(hash === 'en' || hash === 'ru') {
+    if (hash === "en" || hash === "ru") {
         location.reload();
     }
+};
+
+if (!localStorage.lang) localStorage.lang = "en";
+function localRu() {
+    localStorage.lang = "ru";
+}
+function localEn() {
+    localStorage.lang = "en";
 }
 
-if(!localStorage.lang) localStorage.lang = 'en'
-function localRu() {localStorage.lang = 'ru'};
-function localEn() {localStorage.lang = 'en'};
-
 function changeLanguage() {
-    hash = localStorage.lang
+    hash = localStorage.lang;
     for (let key in lang_translate) {
-        let elem = document.querySelector('.' + key);
-        if(elem) {
-            document.querySelector('.' + key).innerHTML = lang_translate[key][hash];
+        let elem = document.querySelector("." + key);
+        if (elem) {
+            document.querySelector("." + key).innerHTML =
+                lang_translate[key][hash];
         }
     }
-    if(hash === 'en') {
-        document.querySelector('.lang_choose_en').style.color='black';
+    if (hash === "en") {
+        document.querySelector(".lang_choose_en").style.color = "black";
     } else {
-        document.querySelector('.lang_choose_ru').style.color='black';
+        document.querySelector(".lang_choose_ru").style.color = "black";
     }
 }
 
@@ -97,23 +103,31 @@ changeLanguage();
 
 //turn button anima
 
-if(localStorage.anima == 'anima-on') document.getElementById('checkbox2').checked = false
-if(localStorage.anima == 'anima-off') document.getElementById('checkbox2').checked = true
+if (!localStorage.anima) localStorage.anima = "anima-on";
+
+if (localStorage.anima == "anima-on")
+    document.getElementById("checkbox2").checked = false;
+if (localStorage.anima == "anima-off")
+    document.getElementById("checkbox2").checked = true;
 
 function turnAnima() {
-    if(document.getElementById('checkbox2').checked == false) {
-        localStorage.anima = 'anima-on';
+    if (document.getElementById("checkbox2").checked == false) {
+        localStorage.anima = "anima-on";
     }
-    if(document.getElementById('checkbox2').checked == true) {
-        localStorage.anima = 'anima-off';
+    if (document.getElementById("checkbox2").checked == true) {
+        localStorage.anima = "anima-off";
     }
     location.reload();
 }
 
 //turn button anima end
 
+//escape
 
+document.addEventListener("keydown", function (e) {
+    if (e.which === 27) {
+        document.getElementById("esc").click();
+    }
+});
 
-
-
-
+//escape end
